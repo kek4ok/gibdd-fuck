dcd4bd0f063b5910ca67ea5582907792701a5cc5
D111sP <115263492+D111sP@users.noreply.github.com>
2022-12-09 17:26:18 +0500

    123

--- clean_img/main.py
+++ clean_img/main.py
@@ -11,7 +11,7 @@ OUTPUT_FOLDER = "readi"
 
 captcha_image_files = glob.glob(os.path.join(CAPTCHA_IMAGE_FOLDER, "*"))
 counts = {}
-
+counts_name = 0
 
 for (i, captcha_image_file) in enumerate(captcha_image_files):
     print("[INFO] processing image {}/{}".format(i + 1, len(captcha_image_files)))
@@ -45,8 +45,8 @@ for (i, captcha_image_file) in enumerate(captcha_image_files):
     image_grey = cv2.cvtColor(ready_img, cv2.COLOR_BGR2GRAY)
     ret, image_TH = cv2.threshold(image_grey, 157, 255, cv2.THRESH_BINARY)
 
-    cv2.imshow("cropped", image_TH)
-    cv2.waitKey(0)
+    #cv2.imshow("cropped", image_TH)
+    #cv2.waitKey(0)
 
 #сохранени
     '''
@@ -59,14 +59,14 @@ for (i, captcha_image_file) in enumerate(captcha_image_files):
     y = 0
     x_max = 0
     y_max = 0
-    for column in range(1,image_TH.shape[1] - 1):
+    for column in range(1, image_TH.shape[1] - 1):
         for row in range(1, image_TH.shape[0] - 1):
             if (image_TH[row, column] == 0):
                 x = column
                 break
         if (x != 0):
             break
-    for row in range(1,image_TH.shape[0] - 1):
+    for row in range(1, image_TH.shape[0] - 1):
         for column in range(1, image_TH.shape[1] - 1):
             if (image_TH[row, column] == 0):
                 y = row
@@ -88,14 +88,123 @@ for (i, captcha_image_file) in enumerate(captcha_image_files):
         if (y_max != 0):
             break
     crop_img_TH = image_TH[y:y_max, x:x_max]
-    cv2.imshow("cropped", crop_img_TH)
-    cv2.waitKey(0)
+
+    #cv2.imshow("cropped", crop_img_TH)
+    #cv2.waitKey(0)
+
     (x_short, y_short, w, h) = cv2.boundingRect(crop_img_TH)
-    print(x_short)
-    print(y_short)
-    print(w)
-    print(h)
     one_img_w = round(w/5)
+    w_rezka_left = 0
+    w_rezka_right = 30
+    array_rezka_left  = []
+    array_rezka_left.append(w_rezka_left)
+    arrCaptcha_name = list(captcha_correct_text)
+    for el in range(5):
+        save_path = os.path.join(OUTPUT_FOLDER, arrCaptcha_name[el])
+
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+
+        img_belia = np.full((50, 30), 255, np.uint8)
+        one_img = crop_img_TH[0:h, el * one_img_w:(el+1)*one_img_w]
+        for row in range(1, one_img.shape[0] - 1):
+            for col in range(1, one_img.shape[1] - 1):
+                img_belia[row, col] = one_img[row, col]
+
+        p = os.path.join(save_path, "{}.png".format(str(counts_name).zfill(6)))
+        cv2.imwrite(p, img_belia)
+        #cv2.imshow("cropped", img_belia)
+        #cv2.waitKey(0)
+        counts_name = counts_name + 1
+    '''
+    for el in range(5):
+        one_img_final = crop_img_TH[0:h, array_rezka_left[el]:array_rezka_left[el+1]]
+        cv2.imshow("cropped", one_img_final)
+        cv2.waitKey(0)
+
+    for el in range(4):
+        one_img = crop_img_TH[0:h, w_rezka_left:w_rezka_left+30]
+        y_hith = 0
+        y_hith_very_smal = 0
+        y_hith_very_big = 0
+        for row in range(1, one_img.shape[0] - 1):
+            for col in range(round(one_img.shape[1] * 0.2),  round(one_img.shape[1] / 3)):
+                if (one_img[row, col] == 0):
+                    y_hith = row
+                    break
+            if (y_hith != 0):
+                break
+        hight_number = h - y_hith
+
+        if (15 >= hight_number):
+            for row in range(1, one_img.shape[0] - 1):
+                for col in range(round((one_img.shape[1] * 2 )/ 3), one_img.shape[1] - round(one_img.shape[1] * 0.1)):
+                    if (one_img[row, col] == 0):
+                        y_hith_very_smal = row
+                        break
+                if (y_hith_very_smal != 0):
+                    break
+            hight_number = h - y_hith_very_smal
+
+        if (45 >= hight_number > 40):
+            for row in range(1, one_img.shape[0] - 1):
+                for col in range(round((one_img.shape[1] * 2 )/ 3), one_img.shape[1] - round(one_img.shape[1] * 0.1)):
+                    if (one_img[row, col] == 0):
+                        y_hith_very_big = row
+                        break
+                if (y_hith_very_big != 0):
+                    break
+
+            hight_number = h - y_hith_very_big
+
+        if(45 >= hight_number > 40):
+            w_rezka_left = w_rezka_left + 30
+        elif(40 >= hight_number > 35):
+            w_rezka_left = w_rezka_left + 27
+        elif (35 >= hight_number > 30):
+            w_rezka_left = w_rezka_left + 23
+        elif (30 >= hight_number > 15):
+            w_rezka_left = w_rezka_left + 15
+
+        w_rezka_right = w_rezka_left + 30
+        array_rezka_left.append(w_rezka_left)
+        #if(w_rezka_left <= w):
+        #    cv2.imshow("cropped", one_img)
+        #   cv2.waitKey(0)
+    #if(w - array_rezka_left[4] > 30):
+    #    array_rezka_left[4] = w - 30
+    #    array_rezka_left.append(w)
+    #else:
+    array_rezka_left.append(w)
+    print(array_rezka_left)
+
+    for el in range(5):
+        one_img_final = crop_img_TH[0:h, array_rezka_left[el]:array_rezka_left[el+1]]
+        cv2.imshow("cropped", one_img_final)
+        cv2.waitKey(0)
+
+        if(15 >= hight_number):
+            for row in range(1, one_img.shape[0] - 1):
+                for col in range(one_img.shape[1] - 10, 24):
+                    if (one_img[row, col] == 0):
+                        y_hith_very_smal = row
+                        break
+                if (y_hith_very_smal != 0):
+                    break
+
+            hight_number = h - y_hith_very_smal
+
+        if (45 >= hight_number > 40):
+            for row in range(1, one_img.shape[0] - 1):
+                for col in range(one_img.shape[1] - 10, 24):
+                    if (one_img[row, col] == 0):
+                        y_hith_very_big = row
+                        break
+                if (y_hith_very_big != 0):
+                    break
+
+            hight_number = h - y_hith_very_big
+
     sdvig = 0
     minsdvig = 0
     lastMove = 0
@@ -130,7 +239,7 @@ for (i, captcha_image_file) in enumerate(captcha_image_files):
         cv2.waitKey(0)
     #pixel_matrix_RGB = image_orig.copy()
 
-
+'''
 '''
     for column in range(1, image.shape[1] - 1):
         for row in range(1, image.shape[0] - 1):
