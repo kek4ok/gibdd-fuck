cad4a4c207e0e77c60daacd1cae16d35245dd2fe
kek4ok <39804639+kek4ok@users.noreply.github.com>
2022-12-12 09:30:13 +0300

    captcha downloader

---
+++ captcha downlloader.py
@@ -0,0 +1,22 @@
+import requests
+import base64
+
+
+def get_iamge_code():
+    response = requests.get('https://check.gibdd.ru/captcha').json()
+    print(response)
+    base_code = response['base64jpg']
+    return base_code
+
+
+def jpg_from_base64(name: str, code):
+    png_recover = base64.b64decode(code)
+    with open(f'{name}.jpg', 'wb') as f:
+        f.write(png_recover)
+        f.close()
+    print(f'File {name}.jpg saved')
+
+
+if __name__ == "__main__":
+    code = get_iamge_code()
+    jpg_from_base64('temp', code)
--- main.py
+++ main.py
@@ -2,22 +2,6 @@ import requests
 import base64
 
 
-def get_iamge_code():
-    response = requests.get('https://check.gibdd.ru/captcha').json()
-    print(response)
-    base_code = response['base64jpg']
-    return base_code
-
-
-def jpg_from_base64(name: str):
-    code = get_iamge_code()
-    png_recover = base64.b64decode(code)
-    with open(f'imgs/{name}.jpg', 'wb') as f:
-        f.write(png_recover)
-        f.close()
-    print(f'File {name}.jpg saved')
-
-
 def get_history(answer: str, token: str):
     url = 'https://xn--b1afk4ade.xn--90adear.xn--p1ai/proxy/check/auto/history'
     payload = {
@@ -29,6 +13,7 @@ def get_history(answer: str, token: str):
     response = requests.post(url, data=payload).json()
 
     return response
+
 
 res = get_history("43954", "SgsJE4YwBcC85kpRbfMbDgQx9buyJUAiKIvxs4E+FMw=")
 print(res.get('code'))
